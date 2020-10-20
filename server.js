7aa0f98836069e397ca4de35f6d8c3a2897026f6
Gonzalo Corsanigo <gonzalocor_10@hotmail.com>
2020-10-20 00:51:19 -0300

    Agregué server.js para que funcione en heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+//Install express server    
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/authapp'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', { root: 'dist/authapp/' });
+});
+
+app.listen(process.env.PORT || 8080);
